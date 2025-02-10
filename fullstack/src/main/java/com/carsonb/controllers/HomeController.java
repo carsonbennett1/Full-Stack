d8ea27aa098bf212dcc616a2e944c99e59fc3cd3
Carson Bennett <carben2004@gmail.com>
2025-02-10 11:40:18 -0800

    Setting up People page with HTML and CSS files

--- fullstack/src/main/java/com/carsonb/controllers/HomeController.java
+++ fullstack/src/main/java/com/carsonb/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package com.carsonb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.carsonb.models.Person;
+
 import org.springframework.ui.Model;
 
 // Class treated by springboot as a place that is controlling other pages
@@ -24,4 +30,19 @@ public class HomeController {
         model.addAttribute("date", "2025-02-09");
         return "helloPage";
     }
+
+    @GetMapping("/people")
+    public String showPeople(Model model){
+        List<Person> people = new ArrayList<Person>();
+        people.add(new Person(0, "Scott", 24));
+        people.add(new Person(1, "Jason", 33));
+        people.add(new Person(2, "Paul", 12));
+        people.add(new Person(3, "Trevor", 42));
+
+        System.out.println(people);
+
+        model.addAttribute("people", people);
+        return "printPeople";
+    }
+
 }
